36f07d28510fd071970a1dc3527b6d453eb501ea
amoghmasur <amoghmasur99@gmail.com>
2024-02-26 09:24:00 +0530

    recent commit

--- app/app/urls.py
+++ app/app/urls.py
@@ -16,9 +16,20 @@ Including another URLconf
 """
 
 # flake8: noqa
+from drf_spectacular.views import(
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+)
+
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/schema/',SpectacularAPIView.as_view(),name='api-schema'),
+    path(
+        'api/docs/',
+        SpectacularSwaggerView.as_view(url_name='api-schema'),
+        name='api-docs',
+    )
 ]
